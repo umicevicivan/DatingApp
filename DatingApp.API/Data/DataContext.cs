c7ce2d910455b8a641db1c45ba0737b8ea087f8f
umicevic.ivan <umicevic.ivan@gmail.com>
2020-08-11 16:42:42 +0200

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        
+        //Ovo ime Values je ime tabele
+        public DbSet<Value> Values { get; set; }
+    }
+}
