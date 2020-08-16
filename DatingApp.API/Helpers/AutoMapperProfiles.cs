a0d4df4667adbfda3308c69530a9b961d35ddf7d
umicevic.ivan <umicevic.ivan@gmail.com>
2020-08-16 16:57:10 +0200

    Ext user class, added dtos, mappers and new migrat

---
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            //Automapper batica po imenu posto su ista zna sta gde treba da mapira, ali u nasem slucaju ne zna za Age i PhotoUrl, to moramo sami da mu kazemo
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+    }
+}
