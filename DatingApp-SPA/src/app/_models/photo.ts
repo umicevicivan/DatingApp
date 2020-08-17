a13d4ec23ad4f75369343d43a7a41cdb060d0a4a
umicevic.ivan <umicevic.ivan@gmail.com>
2020-08-17 16:03:57 +0200

    Retrieving Users from API and added Route resolver

---
+++ DatingApp-SPA/src/app/_models/photo.ts
@@ -0,0 +1,7 @@
+export interface Photo {
+    id: number;
+    url: string;
+    descriptiob: string;
+    datesAdded: Date;
+    inMain: boolean;
+}
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './member-list/member-list.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+
 
 // rute rade po principu na koji prvi naleti, ako ne naleti ni na jedan od ovih pogodice '**'
 // i on ce da ga redirektuje na home, znaci da ta ruta ne postoji na nasoj app
@@ -17,7 +21,8 @@ export const appRoutes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            { path: 'members', component: MemberListComponent},
+            { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
+            { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
             { path: 'messages', component: MessagesComponent},
             { path: 'lists', component: ListsComponent},
         ]
